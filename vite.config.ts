66d5c2191a50fdef4695bb916dd4fa53a52ea12f
Ritesh Birthal <riteshbirthal@gmail.com>
2026-05-27 16:20:56 +0530

    Added GH-Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Pattern-Gallery/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
